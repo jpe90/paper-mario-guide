ce34a0c05f81be1b4681094d48eecdb53ff32fde
jpe90 <patriot.game@gmail.com>
2020-07-13 17:57:26 -0400

    cosmetic background animation implemented

--- lib/widgets/backdrop.dart
+++ lib/widgets/backdrop.dart
@@ -4,6 +4,8 @@ import 'package:logger/logger.dart';
 
 var logger = Logger(printer: PrettyPrinter());
 
+const flingVelocity = 2.0;
+
 class _FrontLayer extends StatelessWidget {
   final Widget child;
   final VoidCallback onTap;
@@ -33,17 +35,73 @@ class Backdrop extends StatefulWidget {
         assert(backLayer != null);
 }
 
-class _BackdropState extends State<Backdrop> {
+class _BackdropState extends State<Backdrop>
+    with SingleTickerProviderStateMixin {
   Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
     return Stack(children: <Widget>[
       widget.backLayer,
-      _FrontLayer(child: widget.frontLayer)
+      PositionedTransition(
+          rect: layerAnimation, child: _FrontLayer(child: widget.frontLayer))
     ]);
+  }
+
+  AnimationController _controller;
+
+  bool get _isFrontLayerVisibile {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+      velocity: _isFrontLayerVisibile ? -flingVelocity : flingVelocity,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var appBar = AppBar(title: Text('Paper Mario'));
+    var appBar = AppBar(
+      elevation: 0,
+      leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            _toggleBackdropLayerVisibility();
+          }),
+      title: Text('Paper Mario'),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: () {
+              _toggleBackdropLayerVisibility();
+            })
+      ],
+    );
 
     return Scaffold(
       appBar: appBar,
