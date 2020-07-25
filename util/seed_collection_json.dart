74770f94dc85b2293c1c7afbe2f7e97841488668
jpe90 <patriot.game@gmail.com>
2020-07-25 14:29:04 -0400

    some progress on collectible json generation

--- util/seed_collection_json.dart
+++ util/seed_collection_json.dart
@@ -4,15 +4,43 @@ import 'dart:io';
 import '../lib/models/collectible.dart';
 
 writeCollectiblesJsonToFile(String path) {
-  List<Collectible> muhCollectibles =
-      createCollectibleList(Level.toadTown, Category.box, 10, 10);
+  List<Collectible> muhCollectibles = [];
   String json = jsonEncode(muhCollectibles);
   print(json);
   new File(path).writeAsString(json);
 }
 
+List<Collectible> createCollectibleListForLevel(
+    {Level level,
+    int startingId,
+    int numHoles,
+    int numToads,
+    int numBoxes,
+    int numTreasures}) {
+  return createCollectibleList(
+          level: level,
+          category: Category.hole,
+          startingId: startingId,
+          numEntries: numHoles) +
+      createCollectibleList(
+          level: level,
+          category: Category.toad,
+          startingId: startingId + numHoles,
+          numEntries: numToads) +
+      createCollectibleList(
+          level: level,
+          category: Category.box,
+          startingId: startingId + numHoles + numToads,
+          numEntries: numBoxes) +
+      createCollectibleList(
+          level: level,
+          category: Category.treasure,
+          startingId: startingId + numHoles + numToads + numBoxes,
+          numEntries: numTreasures);
+}
+
 List<Collectible> createCollectibleList(
-    Level level, Category category, int startingId, int numEntries) {
+    {Level level, Category category, int startingId, int numEntries}) {
   List<Collectible> retList = [];
   for (var i = startingId; i < startingId + numEntries; i++) {
     retList.add(Collectible(
