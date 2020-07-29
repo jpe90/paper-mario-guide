b1f86795499156381628f27043140995470e6cb5
jpe90 <patriot.game@gmail.com>
2020-07-28 20:55:05 -0400

    remy whining
    Former-commit-id: 9277b32566c07aa18193d01cfe87d3883223aa5f

--- util/seed_collection_json.dart
+++ util/seed_collection_json.dart
@@ -108,9 +108,11 @@ List<Collectible> createCollectibleListForLevel(
           numEntries: numTreasures);
 }
 
+// TODO Enhancement 1: take in order here
 List<Collectible> createCollectibleList(
     {Level level, Category category, int startingId, int numEntries}) {
   List<Collectible> retList = [];
+  // TODO enhancement 1: should bookkeep for order field inside this loop
   for (var i = startingId; i < startingId + numEntries; i++) {
     retList.add(notesMap[i] == null
         ? Collectible(
